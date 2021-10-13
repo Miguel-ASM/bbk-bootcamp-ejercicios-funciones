be6758c438f9e4c0cd8ca3b240abacce63d6f5a7
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2021-10-13 16:51:36 +0200

    files ready to start solving the bootdonalds exercise

---
+++ bootdonald/productos.js
@@ -0,0 +1,23 @@
+letproductos = {
+    menus: {
+        'Grand Big Boot': 8.5,
+        'Big Boot': 8,
+        'BootPollo': 7.50,
+        'Cuarto de libra': 7,
+        'BootRoyal Deluxe': 7
+    },
+    burgers: {
+        'Grand Big Boot': 6,
+        'Big Boot': 5.5,
+        'BootPollo': 5,
+        'Cuarto de libra': 4.5,
+        'BootRoyal Deluxe': 4.5
+    },
+    drinks: {
+        'CocaCola': 2,
+        'Fanta': 2,
+        'Agua': 1,
+        'Cerveza': 2.5
+    },
+    fries: 1,
+};
